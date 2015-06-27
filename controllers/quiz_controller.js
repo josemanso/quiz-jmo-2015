036a646b796c6b455863b063b9c7e34ef27f2f66
jose <mansoortega@gmail.com>
2015-06-27 17:24:15 +0200

    despliege DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,26 @@
+// controlador, ahora con DB, importar modelo
+
+var models = require('../models/models.js');
+
 // Get /quizes/question
 exports.question = function(req, res) {
-  res.render('quizes/question', {pregunta: ' Capital de Italia '});
+  //añadimo findAll() para buscar el array de la tabla Quiz
+  models.Quiz.findAll().success(function(quiz) {
+    //res.render('quizes/question', {pregunta: ' Capital de Italia '});
+    res.render('quizes/question', {pregunta: quiz[0].pregunta });
+    })
 };
 
 
 // Get /quizes/answer
 exports.answer = function(req, res) {
-  if (req.query.respuesta === 'Roma'){
-    res.render('quizes/answer', {respuesta: ' Correcto '});
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta){
+      res.render('quizes/answer', {respuesta: ' Correcto '});
   } else {
     res.render('quizes/answer', {respuesta: ' Incorrecto '});
   }
+  })
 };
 
 // get /quizes/creditos/author
