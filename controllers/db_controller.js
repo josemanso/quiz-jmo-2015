3ccfeb807664658c8523a3a9b8e9a8f88ddb63ed
jose <mansoortega@gmail.com>
2015-07-28 13:34:30 +0200

    Quiz mis preguntas, sin image e2

--- controllers/db_controller.js
+++ controllers/db_controller.js
@@ -5,9 +5,12 @@ var models = require('../models/models.js');
 exports.mostrardb = function(req, res) {
   models.Quiz.findAll().then(function(quizdb){
     models.Comment.findAll().then(function(commetdb) {
-      res.render('quizes/mostrardb', {quizes: quizdb,
+      models.User.findAll().then(function(userdb){
+	res.render('quizes/mostrardb', {quizes: quizdb,
 				      comments: commetdb,
+				      users: userdb,
 				      errors: []
+	});
       });
     });
   });
